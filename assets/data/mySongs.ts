063bebf7861196ce69a75e25c6e80d3b2976adbf
ellielom-sheridan <52573597+ellielom-sheridan@users.noreply.github.com>
2019-07-23 13:06:19 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,40 @@
+import { songInfo } from '../../app/songInfo'
+
+export const MYSONGS: songInfo[] = [
+
+    // Song 1
+    {
+        name: "You Don't Know Me",
+        artist: 'Jax Jones ft. RAYE',
+        genre: 'Dance/Electronic',
+        year: 2017,
+        picture: 'assets/images/ydkm.jpg'
+    },
+
+    // Song 2
+    {
+        name: 'Suavemente',
+        artist: 'Elvis Crespo',
+        genre: 'Merengue',
+        year: 1998,
+        picture: 'assets/images/suavamente.jpg'
+    },
+
+    // Song 3
+    {
+        name: 'Santeria',
+        artist: 'Sublime',
+        genre: 'Reggae/Ska',
+        year: 1996,
+        picture: 'assets/images/santeria.jpg'
+    },
+
+    // Song 4
+    {
+        name: 'Clair de Lune',
+        artist: 'Claude Debussy',
+        genre: 'Classical',
+        year: 1890,
+        picture: 'assets/images/clairdelune.jpg'
+    }
+];
